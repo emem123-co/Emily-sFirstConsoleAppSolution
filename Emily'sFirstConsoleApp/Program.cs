7c6045ce6d4ad81ef534bf7b68d5142d47d51c28
Emily Morgan <emorgan921@gmail.com>
2024-06-11 14:50:11 -0400

    Add project files.

---
+++ Emily'sFirstConsoleApp/Program.cs
@@ -0,0 +1,8 @@
+﻿namespace Emily_sFirstConsoleApp;
+
+internal class Program {
+    public static string Name { get; set; } = "Emily"; //property
+
+    static void Main(string[] args) { //method
+        Console.WriteLine($"Hello, {Name} World!");}
+}
